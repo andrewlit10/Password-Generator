a289afa2ae3fcffa739049a8876a9ee0b36b31d8
Andrew Litten <andrew.litten@gmail.com>
2024-06-05 19:20:04 -0700

    added javascript functionality, still working on two different passwords functionality

---
+++ index.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+const passwordLength = 15;
+let firstPassword = document.querySelector(".first-pass");
+let secondPassword = document.querySelector(".second-pass");
+let generateBtn = document.querySelector(".generate-btn");
+
+
+
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+    
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    let randomPassword2 = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter();
+        randomPassword2 += getRandomCharacter();
+    }
+    firstPassword.innerHTML = randomPassword;
+    secondPassword.innerHTML = randomPassword;
+}
+
